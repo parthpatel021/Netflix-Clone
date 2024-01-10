ba49ac0a403dac8b5316bf7c25abfa3606d71f27
Parth Patel <parthkumar111patel@gmail.com>
2024-01-10 11:54:53 +0530

    Protected Routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
 
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if(!session){
+        return {
+            redirect:{
+                destination: '/auth',
+                premanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-2xl">Netflix - Clone</h1>
-    </>
-  )
+    const {data: user} = useCurrentUser();
+
+    return (
+        <>
+            <h1 className="text-2xl text-white">Netflix - Clone</h1>
+            <p className='text-white'>Logged in as: {user?.name}</p>
+            <button className='h-10 w-full bg-white' onClick={() => signOut()}>LogOut</button>
+        </>
+    )
 }
