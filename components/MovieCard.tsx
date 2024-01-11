8460c91c5c03bd0777d8f4f7b9649355f63435e4
Parth Patel <parthkumar111patel@gmail.com>
2024-01-11 20:37:37 +0530

    Video-Player and PlayButton Component Added

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { useRouter } from "next/router";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
 
@@ -7,6 +9,7 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+    const router = useRouter();
     return(
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
 
@@ -24,7 +27,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                 <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
 
                     <div className="flex flex-row items-center gap-3">
-                        <div onClick={() => {}}
+                        <div onClick={() => router.push(`/watch/${data?.id}`)}
                             className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
                         >
                             <BsFillPlayFill size={30} />
